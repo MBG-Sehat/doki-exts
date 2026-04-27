3eb3316b5cf8d75fec429ec46a703ba8b685c547
MBG-Sehat <elvianikhbar33@gmail.com>
2026-04-27 17:44:58 +0700

    Update Doujindesu.kt

--- src/main/kotlin/org/dokiteam/doki/parsers/site/madara/id/Doujindesu.kt
+++ src/main/kotlin/org/dokiteam/doki/parsers/site/madara/id/Doujindesu.kt
@@ -8,7 +8,7 @@ import org.dokiteam.doki.parsers.site.madara.MadaraParser
 
 @MangaSourceParser("DOUJINDESU", "Doujindesu", "id", ContentType.HENTAI)
 internal class Doujindesu(context: MangaLoaderContext) :
-	MadaraParser(context, MangaParserSource.DOUJINDESU, "doujindesu.tv", 20) {
+	MadaraParser(context, MangaParserSource.DOUJINDESU, "doujindesu.tv", 18) {
 	override val tagPrefix = "genres/"
 	override val listUrl = "manga/"
 	override val datePattern = "MMMM d, yyyy"
